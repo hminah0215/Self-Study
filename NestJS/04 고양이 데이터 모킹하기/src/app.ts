fa681e6c5d3524d43ed02481ef3987ba6495ad59
hminah0215 <hyeonminah@gmail.com>
2022-12-19 12:53:01 +0900

    미들웨어 이해하기

--- NestJS/04 고양이 데이터 모킹하기/src/app.ts
+++ NestJS/04 고양이 데이터 모킹하기/src/app.ts
@@ -5,10 +5,42 @@ const app: express.Express = express();
 
 // const data = [1, 2, 3, 4];
 
-app.get('/', (req, res) => {
-  // console.log(req);
-  // res.send({ data });
-  res.send({ cats: Cat });
+// 미들웨어는 순서가 중요!
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('로깅하는 미들웨어닷');
+  next(); // 라우터를 찾도록 next 함수
+});
+
+app.get('/cats/som', (req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('솜이 미들웨어');
+  next();
+});
+
+app.get(
+  '/',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    // console.log(req);
+    console.log(req.rawHeaders[1]);
+    // res.send({ data });
+    res.send({ cats: Cat });
+  },
+);
+
+app.get('/cats/blue', (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  res.send({ error: '404 not found' });
+  next();
 });
 
 app.listen(8000, () => {
